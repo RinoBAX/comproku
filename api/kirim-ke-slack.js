2783b11d0c8d459607bd4bcf6c8d003edaea7d87
rinoBax <aderino123321@gmail.com>
2025-08-20 23:36:44 +0700

    update

--- api/kirim-ke-slack.js
+++ api/kirim-ke-slack.js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
 
   const { nama, email, telepon, subjek, pesan } = request.body;
 
-  const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
+  const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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";
 
   const slackMessage = {
     text: `🔔 Pesan Baru dari Website: ${subjek}`,
